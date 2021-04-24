7ca42ea49884614c3394fa20ac7b298b49fb91ef
parinita <mail@parinita.dev>
2021-04-24 18:01:45 +0530

    added cors origin

--- server.js
+++ server.js
@@ -13,13 +13,18 @@ const dotenv = require('dotenv').config();
 const config = require('./config/config');
 const db = require('./db/dbConnection');
 
-const dbconnection = 'mongodb://127.0.0.1:27017/smm' //config.dbUrl;// //
+const dbconnection = config.dbUrl;
 // 'mongodb://127.0.0.1:27017/smm'
 const PORT = process.env.PORT || 3000;
 
 db.connect(dbconnection, config.options);
 
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://smm-customer-frontend.s3-website.ap-south-1.amazonaws.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
