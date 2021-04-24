ff52b57dfb9b5f7a1ac0baf458706e1215484cdd
parinita <mail@parinita.dev>
2021-04-24 18:11:57 +0530

    cors configuration change

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ global.HTTP_STATUS = require('http-status');
 
 const express = require('express');
 const app = express();
-const cors = require('cors')
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const config = require('./config/config');
@@ -19,13 +18,16 @@ const PORT = process.env.PORT || 3000;
 
 db.connect(dbconnection, config.options);
 
-app.use(cors());
 app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://smm-customer-frontend.s3-website.ap-south-1.amazonaws.com");
     // res.header("Access-Control-Allow-Origin", "http://smm-customer-frontend.s3-website.ap-south-1.amazonaws.com");
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Request-Headers", "*");
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization, accesstoken");
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 });
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
